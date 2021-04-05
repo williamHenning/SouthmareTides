648dbe62d37fb0b5492cb6705dcc23a90503b8f4
Haoyu Zhang <hzwr@outlook.com>
2021-04-05 14:52:07 -0400

    Make health bar facing the camera

---
+++ Assets/Scripts/UI/HealthbarBillboard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthbarBillboard : MonoBehaviour
+{
+    public Transform cam;
+
+    private void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera").transform;
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + cam.forward);
+    }
+}
